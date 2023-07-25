7c5def860929d6f37a97cde178920a868ce40a55
Mark <fitdadfatgirl@gmail.com>
2023-07-25 15:58:12 +0700

    add greetings folder

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 
+	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
+	var message string
+	message = greetings.Hello("Gladys")
+	fmt.Println(message)
 	fmt.Println(quote.Go())
 }
